cf6d08e1d3afece60b4bb262bf021826afce49a3
JaeTriesGit <boffering@hotmail.com>
2023-08-30 14:56:39 +0300

    aaa

--- frontend/src/comps/Countries/Info.jsx
+++ frontend/src/comps/Countries/Info.jsx
@@ -1,6 +1,9 @@
 export default function Info({Data, Close}){
     const Lang = Object.values(Data.languages)
+    const Curr = Object.values(Data.currencies)
+
     const Languages = Lang.map(obj => <p key={obj}>{obj}</p>)
+    const Currencies = Curr.map(obj => <p key={obj}>{obj}</p>)
     return(
         <div className='Country-Modal'>
             <button onClick={Close} className='Close'>X</button>
@@ -11,6 +14,10 @@ export default function Info({Data, Close}){
                     <div className='Languages'>
                         <p><span className='TitleSpan'>Languages:</span></p>
                         {Languages}
+                    </div>
+                    <div className='Currencies'>
+                        <p><span className='TitleSpan'>Currencies: </span></p>
+                        {Currencies}
                     </div>
                 </div>
             </div>
